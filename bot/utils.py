6b64b2dbcdf21e83c8d4b78aade3ea813b009cb8
VIRUSGAMING64 <virusroger6@gmail.com>
2022-11-13 07:25:59 -0500

    update bot 1.02

--- bot/utils.py
+++ bot/utils.py
@@ -29,8 +29,42 @@ def dirs(s):
     else:
         return ""
 
+def sysinfo(s):
+    if(s == '/system info'):
+        try:
+            os.system('systeminfo > m')
+            f = open('m')
+            line = f.read(65536)
+            msg = line
+            while(line):
+                line = f.read(65536)
+                msg+=line
+            return msg
+        except:
+            return "access denied"
+    else:
+        return ""
+
+def helps(s):
+    if(s=='/help'):
+        m = [
+            "/sendfile",
+            "/cd",
+            "/dir",
+            "/chdir",
+            "/system info"
+        ]
+        msg = ""
+        for i in m:
+            msg+=i+"\n"
+        return msg
+    else:
+        return ""
+
 def command(s):
     x = ""
+    x+= helps(s)
     x += dir(s)
     x += dirs(s)
+    x+= sysinfo(s)
     return x
