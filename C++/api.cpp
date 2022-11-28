2fb1603ea70e1d35ebfd18731792d1ac2295afa6
VIRUSGAMING64 <virusroger6@gmail.com>
2022-11-28 12:48:37 -0500

    Sorted

--- C++/api.cpp
+++ C++/api.cpp
@@ -6,7 +6,7 @@
 #define CHUNK_SIZE 131072
 using namespace std;
 using namespace filesystem;
-queue<pair<string, string>> WRITE_QUEUE;
+queue<pair<string,string>> WRITE_QUEUE;
 mutex mtx;
 atomic<int> threads;
 vector<thread> ThreadPool;
@@ -29,14 +29,14 @@ string getcwd()
     GetCurrentDirectoryA(100,dira);
     LPCSTR a = "";
     bool b = true;
-    for(int i=sizeof(dira)-1; i>=0; i--)
+    for(int i = sizeof(dira) - 1; i >= 0; i--)
     {
-        if(dira[i]!=' '){b=false;}
+        if(dira[i] != ' '){ b = false; }
         if(b)continue;
-        a+=dira[i];
+        a += dira[i];
     }
     string dir = dira;
-    return dir;   
+    return dir;
 }
 
 void chdir(string dir)
@@ -47,14 +47,14 @@ void chdir(string dir)
 
 string GetFileDirectory(string file)
 {
-    string aux="";
+    string aux = "";
     bool f = false;
-    for(int i=file.size(); i >= 0;i--)
+    for(int i = file.size(); i >= 0;i--)
     {
-        if(file[i] == '\\' || file[i] == '/')f=true;
+        if(file[i] == '\\' || file[i] == '/')f = true;
         if(f)
         {
-            aux = file[i]+aux;
+            aux = file[i] + aux;
         }
     }
     return aux;
@@ -62,11 +62,11 @@ string GetFileDirectory(string file)
 
 string GetFileName(string rute)
 {
-    int  i = rute.size()-1;
+    int  i = rute.size() - 1;
     string s = "";
-    while(rute[i]!='\\' && rute[i]!='/')
+    while(rute[i] != '\\' && rute[i] != '/')
     {
-        s=rute[i] + s;
+        s = rute[i] + s;
         i--;
     }
     return s;
@@ -79,10 +79,10 @@ string GetLastDirectory(string rute,bool file = false)
         rute = GetFileDirectory(rute);
     }
     int n = rute.size();
-    if(rute.size()<=2)return "";
-    if(rute[n-1] == '\\' || rute[n-1] == '/')
+    if(rute.size() <= 2)return "";
+    if(rute[n - 1] == '\\' || rute[n - 1] == '/')
     {
-        if(rute[n-2] == ':')return rute;
+        if(rute[n - 2] == ':')return rute;
         rute.pop_back();
     }
     rute = GetFileName(rute);
@@ -101,29 +101,29 @@ void CPfile(string file,string fileto)
     threads++;
     FILE* f = fopen(file.c_str(),"rb");
     FILE* ft = fopen(fileto.c_str(),"wb");
-    size_t 
-    Chunks,
-    ResChunks;
+    size_t
+        Chunks,
+        ResChunks;
     try
     {
         auto size1 = GetFileSize(file);
-        Chunks = size1/CHUNK_SIZE;
-        ResChunks = size1%CHUNK_SIZE;
+        Chunks = size1 / CHUNK_SIZE;
+        ResChunks = size1 % CHUNK_SIZE;
     }
     catch(exception a)
     {
-        cerr << file <<" no is file  " << a.what() << endl;
+        cerr << file << " no is file  " << a.what() << endl;
         return;
     }
 
-    while(Chunks!=0)
+    while(Chunks != 0)
     {
         char buff[CHUNK_SIZE];
         memset(buff,0,CHUNK_SIZE);
         fread(buff,sizeof(buff),1,f);
         fwrite(buff,sizeof(buff),1,ft);
         Chunks--;
-    }    
+    }
     char buff[ResChunks];
     memset(buff,0,ResChunks);
     fread(buff,sizeof(buff),1,f);
@@ -131,7 +131,7 @@ void CPfile(string file,string fileto)
     fclose(f);
     fclose(ft);
     mtx.lock();
-    cout << file << "  --->  " << fileto << "   writer: "<< GetThisProcessID() <<endl;
+    cout << file << "  --->  " << fileto << "   writer: " << GetThisProcessID() << endl;
     threads--;
     mtx.unlock();
 }
@@ -150,7 +150,7 @@ void __fastcall Copy(string A,string B)
     try
     {
         directory_iterator ITER(A);
-        for(auto&i:ITER)
+        for(auto& i : ITER)
         {
             string aux_dir = i.path().string();
             if(i.is_directory())
@@ -158,29 +158,29 @@ void __fastcall Copy(string A,string B)
                 try
                 {
                     if(GetFileName(aux_dir) == "$RECYCLE.BIN")
-                    continue;
+                        continue;
                     A = aux_dir;
                     chdir(B);
                     CreateDirectory(GetLastDirectory(aux_dir));
-                    string 
-                    AX=A,
-                    BX=B+'/'+GetLastDirectory(aux_dir);
+                    string
+                        AX = A,
+                        BX = B + '/' + GetLastDirectory(aux_dir);
                     thread(Copy,AX,BX).join();
                 }
-                catch(exception &a)
+                catch(exception& a)
                 {
                     cerr << "Error creating directory " << aux_dir << endl;
                     cerr << a.what() << endl;
                 }
             }
             else
-            {        
+            {
                 A = aux_dir;
-                string 
-                AX = A, 
-                BX = B+'\\'+GetFileName(aux_dir);
+                string
+                    AX = A,
+                    BX = B + '\\' + GetFileName(aux_dir);
                 WRITE_QUEUE.push({AX,BX});
-            } 
+            }
         }
     }
     catch(filesystem_error a)
@@ -203,7 +203,7 @@ HANDLE CreateFile(string name,int mode,int share_mode,int CreateConfiguration,in
     );
     return x;
 }
-void c(string a, string b)
+void c(string a,string b)
 {
     cout << "2 strings\n";
 }
@@ -214,24 +214,12 @@ void th()
     ID = GetThisProcessID();
     while(true)
     {
-        Sleep(1000);
+        Sleep(100);
         Beep(1100,100);
     }
 }
 
 int main()
-{   
-    thread(th).detach();
-    Sleep(1000);
-    cout << "Closing...   " << ID <<endl;
-    HANDLE hand = OpenProcess(1,true,ID);
-    if(hand == NULL)
-    {
-        cerr << "not open process" << endl;
-    }
-    else
-    {
-        auto c = TerminateProcess(hand,0);
-        cout << c << endl;
-    }    
+{
+    
 }
