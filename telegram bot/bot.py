7f4cea631f1337f05ec92839fabdbe52d4a68698
VIRUSGAMING64 <96099442+VIRUSGAMING64@users.noreply.github.com>
2023-08-16 00:25:32 -0400

    1.1.0.2

--- telegram bot/bot.py
+++ telegram bot/bot.py
@@ -1,4 +1,5 @@
 from utils import *
+
 # GLOBAL VARIABLES
 API_ID = 29695292
 API_HASH = "8b05ce00146edeeae7aafc4bea30e713"
@@ -15,59 +16,73 @@ MAX_MESSAGE_LENGTH = 4096
 WORKERS = 16
 ASKING = 0
 DOWNLOADER = ""
-bot = Client("bot", API_ID, API_HASH, workers=WORKERS)  
+bot = Client("bot", API_ID, API_HASH, workers=WORKERS)
 LAST_MEDIA_DOWNLOADED = ""
 log = loger("log_file.log")
-TotalUsers = log.read() 
+TotalUsers = log.read()
 LAST_MESSAGE_DOWNLOAD = ""
 # END GLOBAL VARIABLES
 
+
 async def progres(current, total):
-    global LAST_MESSAGE_DOWNLOAD,LAST_MEDIA_DOWNLOADED,DOWNLOADER
+    global LAST_MESSAGE_DOWNLOAD, LAST_MEDIA_DOWNLOADED, DOWNLOADER
     try:
         s = f"{current * 100 / total:.1f}%"
         porcent = current * 100 / total
-        MSG = str(str("* " * int(int(porcent) // 10)) + str(". " * (10 - (int(porcent) // 10))) + "\n" + str(s))
+        MSG = str(
+            str("* " * int(int(porcent) // 10))
+            + str(". " * (10 - (int(porcent) // 10)))
+            + "\n"
+            + str(s)
+        )
         MSG = Fill_Border(MSG)
         print(MSG)
-        if(LAST_MESSAGE_DOWNLOAD == ""):
+        if LAST_MESSAGE_DOWNLOAD == "":
             try:
-                LAST_MESSAGE_DOWNLOAD = await bot.send_message(DOWNLOADER,MSG)   
+                LAST_MESSAGE_DOWNLOAD = await bot.send_message(DOWNLOADER, MSG)
             except Exception as e:
-                print('error on first msg: ',e)
+                print("error on first msg: ", e)
         else:
             try:
-                await bot.edit_message_text(chat_id=LAST_MESSAGE_DOWNLOAD.chat.id,message_id=LAST_MESSAGE_DOWNLOAD.id,text=MSG)
+                await bot.edit_message_text(
+                    chat_id=LAST_MESSAGE_DOWNLOAD.chat.id,
+                    message_id=LAST_MESSAGE_DOWNLOAD.id,
+                    text=MSG,
+                )
             except:
                 print("error on edit")
     except:
         print("error on id for download")
 
-async def Download(message,user_id):
+
+async def Download(message, user_id):
     global DOWNLOADER
     try:
-        if(DOWNLOADER != ""):
+        if DOWNLOADER != "":
             return
     except:
         print("error on check current download")
     try:
         DOWNLOADER = user_id
-        LAST_MEDIA_DOWNLOADED = await bot.download_media(message,progress=progres)
-        await bot.delete_messages(LAST_MESSAGE_DOWNLOAD.chat.id,LAST_MESSAGE_DOWNLOAD.id)
+        LAST_MEDIA_DOWNLOADED = await bot.download_media(message, progress=progres)
+        await bot.delete_messages(
+            LAST_MESSAGE_DOWNLOAD.chat.id, LAST_MESSAGE_DOWNLOAD.id
+        )
     except Exception as e:
         DOWNLOADER = ""
         if str(e) == "This message doesn't contain any downloadable media":
             DOWNLOADER = ""
-        else: 
+        else:
             print(e)
     finally:
         LAST_MESSAGE_DOWNLOAD = ""
         DOWNLOADER = ""
 
+
 @bot.on_message()
 async def on_message(client, message):
-    global WAITING_FOR_FILENAME, log, CHANGE_DIR, CMD, TotalUsers, ALERT, WRITING,GETING_FILENAME
-    global ASKING,NOTEPAD_FILENAME,ACTUAL_MESSAGE
+    global WAITING_FOR_FILENAME, log, CHANGE_DIR, CMD, TotalUsers, ALERT, WRITING, GETING_FILENAME
+    global ASKING, NOTEPAD_FILENAME, ACTUAL_MESSAGE
     cmd = ""
     chat_type = message.chat.type
     ACTUAL_MESSAGE = message
@@ -75,12 +90,12 @@ async def on_message(client, message):
     rute = os.getcwd()
     msg = message.text
     user = message.from_user.first_name
-    await Download(message,ID)
-    
+    await Download(message, ID)
+
     if msg == None:
-        return #no text in message
-    
-    #check if user is new
+        return  # no text in message
+
+    # check if user is new
     data = "[" + str(user) + "," + str(ID) + "]"
     find = 0
     if data in TotalUsers:
@@ -89,32 +104,32 @@ async def on_message(client, message):
         print("User: " + user + " is new")
         log.write(data)
         TotalUsers = log.read()
-    #endregion
+    # endregion
 
-    #notepad mode
+    # notepad mode
     if msg == "/notepad":
         WRITING = not WRITING
         if WRITING:
-            await bot.send_message(ID, "Notepad mode on...")            
+            await bot.send_message(ID, "Notepad mode on...")
             await bot.send_message(ID, "Send filename please")
             GETING_FILENAME = 1
         else:
             await bot.send_message(ID, "Notepad mode off...")
         return
     if WRITING:
-        if(GETING_FILENAME):
+        if GETING_FILENAME:
             GETING_FILENAME = 0
             NOTEPAD_FILENAME = msg
-            file = open(msg,"w")
+            file = open(msg, "w")
             file.close()
-            return 
-        file = open(NOTEPAD_FILENAME,"a")
+            return
+        file = open(NOTEPAD_FILENAME, "a")
         file.write(msg)
         file.close()
         return
-    #endregion
+    # endregion
 
-    #cambia a consola y biseversa
+    # cambia a consola y biseversa
     if msg == "/chmod":
         CMD = not CMD
         if CMD:
@@ -122,8 +137,7 @@ async def on_message(client, message):
         else:
             await bot.send_message(ID, "Chat mode change to normal bot")
         return
-    #endregion
-
+    # endregion
 
     if msg == "/alert":
         if user != "VIRUSGAMING":
@@ -142,7 +156,6 @@ async def on_message(client, message):
                 continue
             await bot.send_message(M[i][1], msg)
         return
-
 
     if msg == "/getlog":
         datas = GetLog()
@@ -181,9 +194,9 @@ async def on_message(client, message):
 
     elif WAITING_FOR_FILENAME == True:
         WAITING_FOR_FILENAME = False
-        TEMP = await bot.send_message(ID,"uploading to telegram...")
+        TEMP = await bot.send_message(ID, "uploading to telegram...")
         await bot.send_document(ID, msg)
-        await bot.delete_messages(TEMP.chat.id,TEMP.id)
+        await bot.delete_messages(TEMP.chat.id, TEMP.id)
 
     elif message.text == "/sendfile":
         await bot.send_message(ID, "Send file name")
@@ -202,8 +215,10 @@ async def on_message(client, message):
 async def on_edited_message(client, message):
     await on_message(client, message)
 
+
 @bot.on_deleted_messages()
 async def on_deleted_messages(client, message):
     bot.send_message(message.chat.id, "lo borraste...")
+
 
 bot.run()
